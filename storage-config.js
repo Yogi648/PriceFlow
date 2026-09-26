cb1ccb53c96830b25e47c9634c4be3741eae2c67
Yogi648 <yogeshkumarkurella@gmail.com>
2026-09-26 14:20:53 +0530

    Update storage-config.js

--- storage-config.js
+++ storage-config.js
@@ -2,5 +2,8 @@
    Set only a public endpoint and, if your API uses one, a public client token.
    Never put a database password, service-role key, or cloud secret here.
 */
-window.PRICEFLOW_STORAGE_API_URL = '';
-window.PRICEFLOW_STORAGE_API_KEY = '';
+window.PRICEFLOW_STORAGE_API_URL =
+  'https://priceflow-storage-api.yogi648-priceflow.workers.dev';
+
+window.PRICEFLOW_STORAGE_API_KEY =
+  'PriceFlowPublicClient-2026';
